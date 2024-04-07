206d57e5ebb23bba76fc53aafe090152ea3e02e6
lukestahl <stahl.luke@web.de>
2024-04-07 22:36:35 +0200

    update env mapping

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { env } from "hono/adapter";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
 
@@ -8,13 +9,14 @@ import type { AppContext } from "./context";
 import { AuthController } from "./controller/auth/auth.controller";
 import { UserController } from "./controller/user/user.controller";
 import { initalizeDB } from "./database/db";
+import { Env } from "./env";
 
 const app = new Hono<AppContext>();
-
+console.log(process.env.WEB_DOMAIN);
 app
   .use(logger())
   .use((c, next) => {
-    const handler = cors({ origin: c.env.WEB_DOMAIN });
+    const handler = cors({ origin: env<Env>(c).WEB_DOMAIN });
     return handler(c, next);
   })
   .use((c, next) => {
