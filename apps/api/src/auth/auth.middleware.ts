40eb89870667ee8ee715ab946b59b0b8e389e586
lukestahl <stahl.luke@web.de>
2024-04-09 22:19:29 +0200

    fix verify

--- apps/api/src/auth/auth.middleware.ts
+++ apps/api/src/auth/auth.middleware.ts
@@ -14,9 +14,11 @@ export const AuthMiddleware = async (c: Context<AppContext>, next: () => Promise
 
   const originHeader = c.req.header("Origin") ?? c.req.header("origin");
   const hostHeader = c.req.header("Host") ?? c.req.header("X-Forwarded-Host");
+
   if (
-    (!originHeader || !hostHeader || !verifyRequestOrigin(originHeader, [hostHeader, env(c).API_DOMAIN])) &&
-    env(c).WORKER_ENV === "production"
+    (!originHeader || !hostHeader || !verifyRequestOrigin(originHeader, [hostHeader, env(c).WEB_DOMAIN])) &&
+    env(c).WORKER_ENV === "production" &&
+    c.req.method !== "GET"
   ) {
     return new Response(null, {
       status: 403,
