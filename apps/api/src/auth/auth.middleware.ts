f3fd049a98b39e2473b82b6c2cf1df2df1316ca8
lukestahl <stahl.luke@web.de>
2024-04-09 20:31:43 +0200

    fix verify

--- apps/api/src/auth/auth.middleware.ts
+++ apps/api/src/auth/auth.middleware.ts
@@ -12,12 +12,8 @@ export const AuthMiddleware = async (c: Context<AppContext>, next: () => Promise
   }
   const lucia = c.get("lucia");
 
-  const originHeader = c.req.header("Origin");
+  const originHeader = c.req.header("Origin") ?? c.req.header("origin");
   const hostHeader = c.req.header("Host") ?? c.req.header("X-Forwarded-Host");
-  console.log("c.req.header", c.req.header());
-  console.log("originHeader", originHeader);
-  console.log("hostHeader", hostHeader);
-  console.log(env(c).WORKER_ENV);
   if (
     (!originHeader || !hostHeader || !verifyRequestOrigin(originHeader, [hostHeader])) &&
     env(c).WORKER_ENV === "production"
